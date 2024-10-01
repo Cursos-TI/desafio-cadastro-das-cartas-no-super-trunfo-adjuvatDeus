09e931757ad1493251e23234a420d7c14b312bab
adjuvatDeus <lucas.santos_2011@hotmail.com>
2024-10-01 15:20:50 +0000

    card

---
+++ castas.c
@@ -0,0 +1,211 @@
+#include <stdio.h> 
+ 
+ int main(){
+   
+     printf("super trunfo card game \n");
+     
+     printf("estado:[A] \n");
+     printf("cidade:[A01] \n");
+     printf("População:[15000] \n");
+     printf("Área:[10000] \n");
+     printf("PIB:[514566] \n");
+     printf("Número de pontos turísticos:[21] \n");
+
+     printf("cidade:[A02] \n");
+     printf("População:[25000] \n");
+     printf("Área:[180000] \n");
+     printf("PIB:[9756621] \n");
+     printf("Número de pontos turísticos:[42] \n");
+     
+     printf("cidade:[A03] \n");
+     printf("População:[37522] \n");
+     printf("Área:[199000] \n");
+     printf("PIB:[8999621] \n");
+     printf("Número de pontos turísticos:[57] \n");
+    
+     printf("cidade:[A04] \n");
+     printf("População:[49770] \n");
+     printf("Área:[258450] \n");
+     printf("PIB:[19865621] \n");
+     printf("Número de pontos turísticos:[75] \n");
+    
+     printf("estado:[B] \n");
+     printf("cidade:[B01] \n");
+     printf("População:[36500] \n");
+     printf("Área:[350000] \n");
+     printf("PIB:[6587416] \n");
+     printf("Número de pontos turísticos:[36] \n"); 
+
+     printf("cidade:[B02] \n");
+     printf("População:[41500] \n");
+     printf("Área:[359000] \n");
+     printf("PIB:[9757821] \n");
+     printf("Número de pontos turísticos:[42] \n");
+     
+     printf("cidade:[B03] \n");
+     printf("População:[56622] \n");
+     printf("Área:[475600] \n");
+     printf("PIB:[9759621] \n");
+     printf("Número de pontos turísticos:[58] \n");
+    
+     printf("cidade:[B04] \n");
+     printf("População:[47570] \n");
+     printf("Área:[325450] \n");
+     printf("PIB:[1005621] \n");
+     printf("Número de pontos turísticos:[43] \n");
+    
+     printf("estado:[C] \n");
+     printf("cidade:[C01] \n");
+     printf("População:[45500] \n");
+     printf("Área:[350400] \n");
+     printf("PIB:[777566] \n");
+     printf("Número de pontos turísticos:[36] \n");
+
+     printf("cidade:[C02] \n");
+     printf("População:[23540] \n");
+     printf("Área:[223200] \n");
+     printf("PIB:[1754621] \n");
+     printf("Número de pontos turísticos:[17] \n");
+     
+     printf("cidade:[C03] \n");
+     printf("População:[11122] \n");
+     printf("Área:[195660] \n");
+     printf("PIB:[2331621] \n");
+     printf("Número de pontos turísticos:[11] \n");
+    
+     printf("cidade:[C04] \n");
+     printf("População:[59670] \n");
+     printf("Área:[389450] \n");
+     printf("PIB:[7564521] \n");
+     printf("Número de pontos turísticos:[68] \n");
+    
+     printf("estado:[D] \n");
+     printf("cidade:[D01] \n");
+     printf("População:[19000] \n");
+     printf("Área:[358000] \n");
+     printf("PIB:[1568566] \n");
+     printf("Número de pontos turísticos:[28] \n");
+
+     printf("cidade:[D02] \n");
+     printf("População:[25978] \n");
+     printf("Área:[335000] \n");
+     printf("PIB:[3986354] \n");
+     printf("Número de pontos turísticos:[31] \n");
+     
+     printf("cidade:[D03] \n");
+     printf("População:[30054] \n");
+     printf("Área:[398000] \n");
+     printf("PIB:[5648874] \n");
+     printf("Número de pontos turísticos:[36] \n");
+    
+     printf("cidade:[D04] \n");
+     printf("População:[412547] \n");
+     printf("Área:[456957] \n");
+     printf("PIB:[6587692] \n");
+     printf("Número de pontos turísticos:[46] \n");
+    
+     printf("estado:[E] \n");
+     printf("cidade:[E01] \n");
+     printf("População:[24890] \n");
+     printf("Área:[359800] \n");
+     printf("PIB:[7587416] \n");
+     printf("Número de pontos turísticos:[19] \n");
+
+     printf("cidade:[E02] \n");
+     printf("População:[15995] \n");
+     printf("Área:[198600] \n");
+     printf("PIB:[2000578] \n");
+     printf("Número de pontos turísticos:[14] \n");
+     
+     printf("cidade:[E03] \n");
+     printf("População:[64589] \n");
+     printf("Área:[459600] \n");
+     printf("PIB:[9997561] \n");
+     printf("Número de pontos turísticos:[65] \n");
+    
+     printf("cidade:[E04] \n");
+     printf("População:[47333] \n");
+     printf("Área:[398845] \n");
+     printf("PIB:[5669001] \n");
+     printf("Número de pontos turísticos:[51] \n");
+    
+     printf("estado:[F] \n");
+     printf("cidade:[F01] \n");
+     printf("População:[32110] \n");
+     printf("Área:[260400] \n");
+     printf("PIB:[3548566] \n");
+     printf("Número de pontos turísticos:[29] \n");
+
+     printf("cidade:[F02] \n");
+     printf("População:[16580] \n");
+     printf("Área:[229988] \n");
+     printf("PIB:[2985621] \n");
+     printf("Número de pontos turísticos:[23] \n");
+     
+     printf("cidade:[F03] \n");
+     printf("População:[19833] \n");
+     printf("Área:[265865] \n");
+     printf("PIB:[3256621] \n");
+     printf("Número de pontos turísticos:[24] \n");
+    
+     printf("cidade:[F04] \n");
+     printf("População:[71598] \n");
+     printf("Área:[526450] \n");
+     printf("PIB:[25644521] \n");
+     printf("Número de pontos turísticos:[85] \n");
+    
+     printf("estado:[G] \n");
+     printf("cidade:[G01] \n");
+     printf("População:[30218] \n");
+     printf("Área:[297500] \n");
+     printf("PIB:[3548566] \n");
+     printf("Número de pontos turísticos:[36] \n");
+    
+     printf("cidade:[G02] \n");
+     printf("População:[49570] \n");
+     printf("Área:[385558] \n");
+     printf("PIB:[3446621] \n");
+     printf("Número de pontos turísticos:[33] \n");
+     
+     printf("cidade:[G03] \n");
+     printf("População:[14225] \n");
+     printf("Área:[267545] \n");
+     printf("PIB:[1564333] \n");
+     printf("Número de pontos turísticos:[13] \n");
+    
+     printf("cidade:[G04] \n");
+     printf("População:[9548] \n");
+     printf("Área:[99450] \n");
+     printf("PIB:[44521] \n");
+     printf("Número de pontos turísticos:[8] \n");
+    
+     printf("estado:[H] \n");
+     printf("cidade:[H01] \n");
+     printf("População:[46522] \n");
+     printf("Área:[426500] \n");
+     printf("PIB:[5558466] \n");
+     printf("Número de pontos turísticos:[45] \n");
+    
+     printf("cidade:[H02] \n");
+     printf("População:[57894] \n");
+     printf("Área:[587002] \n");
+     printf("PIB:[9894421] \n");
+     printf("Número de pontos turísticos:[56] \n");
+     
+     printf("cidade:[H03] \n");
+     printf("População:[80799] \n");
+     printf("Área:[79945] \n");
+     printf("PIB:[23864333] \n");
+     printf("Número de pontos turísticos:[96] \n");
+    
+     printf("cidade:[H04] \n");
+     printf("População:[89999] \n");
+     printf("Área:[999741] \n");
+     printf("PIB:[35005145] \n");
+     printf("Número de pontos turísticos:[132] \n");
+    
+    
+    return 0;
+}
+  
+ 
